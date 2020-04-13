e01bee77347e6a9fd80cfc2526cf626ea2138677
Zachary <zachjoliver@gmail.com>
2020-04-12 17:27:43 -0700

    All specs working except presummably bounce back calculation

--- snakes_ladders.js
+++ snakes_ladders.js
@@ -17,12 +17,12 @@ function SnakesLadders() {
 
 SnakesLadders.prototype.play = function (die1, die2) {
   if (this.won) return 'Game over!';
-
   let pos = this.currP === 1 ? this.p1Pos : this.p2Pos;
-
   pos += die1 + die2;
+
   if (this.ladders[pos]) pos = this.ladders[pos];
   if (this.snakes[pos]) pos = this.snakes[pos];
+
   if (pos === 100) {
     this.won = true;
   } else if (pos > 100) {
@@ -34,44 +34,41 @@ SnakesLadders.prototype.play = function (die1, die2) {
   if (this.won) return `Player ${this.currP} Wins!`;
   if (die1 !== die2) this.currP = this.currP === 1 ? 2 : 1;
 
-  // if (this.currP === 1) {
-  //   this.p1Pos += die1 + die2;
-  //   if (this.ladders[this.p1Pos]) this.p1Pos = this.ladders[this.p1Pos];
-  //   if (this.snakes[this.p1Pos]) this.p1Pos = this.snakes[this.p1Pos];
-  //   if (this.p1Pos === 100) {
-  //     this.won === true;
-  //     return `Player ${1} Wins!`;
-  //   } else if (this.p1Pos > 100) {
-  //     this.p1Pos = 100 - (this.p1Pos - 100);
-  //     if (this.snakes[this.p1Pos]) this.p1Pos = this.snakes[this.p1Pos];
-  //   }
-  //   if (die1 !== die2) this.currP = 2;
-  // } else {
-  //   this.p2Pos += die1 + die2;
-  //   if (this.ladders[this.p2Pos]) this.p2Pos = this.ladders[this.p2Pos];
-  //   if (this.snakes[this.p2Pos]) this.p2Pos = this.snakes[this.p2Pos];
-  //   if (this.p2Pos === 100) {
-  //     this.won === true;
-  //     return `Player ${2} Wins!`;
-  //   } else if (this.p2Pos > 100) {
-  //     this.p2Pos = 100 - (this.p2Pos - 100);
-  //     if (this.snakes[this.p2Pos]) this.p2Pos = this.snakes[this.p2Pos];
-  //   }
-  //   if (die1 !== die2) this.currP = 1;
-  // }
-
   // if (die2 === 5) debugger;
-  let retPlayer = die1 !== die2 && this.currP !== 2 ? 2 : 1;
-  let retPos = die1 !== die2 && this.currP !== 2 ? this.p2Pos : this.p1Pos;
+  let retPlayer;
+  let retPos;
+  if (die1 !== die2) {
+    retPlayer = currP === 2 ? 2 : 1;
+    retPos = currP === 2 ? this.p2Pos : this.p1Pos;
+  } else {
+    retPlayer = currP === 2 ? 1 : 2;
+    retPos = currP === 2 ? this.p1Pos : this.p2Pos;
+  }
+  // let retPlayer = die1 !== die2 && this.currP !== 2 ? 2 : 1;
+  // let retPos = die1 !== die2 && this.currP !== 2 ? this.p2Pos : this.p1Pos;
   return `Player ${retPlayer} is on square ${retPos}`;
 }
 
 var game = new SnakesLadders();
-const result1 = game.play(1, 1) //"Player 1 is on square 38", "Should return: 'Player 1 is on square 38'");
-game.play(1,5);
-game.play(6,2);
-game.play(1,1);
-console.log(result1);
+game.play(1, 4) //"Player 1 is on square 38", "Should return: 'Player 1 is on square 38'");
+game.play(6, 1);
+game.play(6, 1);
+game.play(6, 4);
+game.play(5, 3);
+game.play(3, 5);
+game.play(6, 3);
+game.play(6, 5);
+game.play(6, 2);
+game.play(6, 3);
+game.play(3, 1);
+game.play(4, 3);
+game.play(3, 4);
+game.play(6, 3);
+game.play(4, 2);
+game.play(1, 2);
+game.play(1, 3);
+game.play(1, 5);
+game.play(3, 3);
 // Test.assertEquals(game.play(1, 5), "Player 1 is on square 44", "Should return: 'Player 1 is on square 44'");
 // Test.assertEquals(game.play(6, 2), "Player 2 is on square 31", "Should return: 'Player 2 is on square 31'");
 // Test.assertEquals(game.play(1, 1), "Player 1 is on square 25", "Should return: 'Player 1 is on square 25'");
