0d43216eae14a7e49a58d14794cdea88ec8f4d91
Zachary <zachjoliver@gmail.com>
2020-04-14 16:55:07 -0700

    Add chess color problem and naive solution with comments

---
+++ chessColors.js
@@ -0,0 +1,18 @@
+function chessBoardCellColor(cell1, cell2) {
+  const alpha = 'ABCDEFGH'; 
+  const cell1Black = (alpha.indexOf(cell1[0]) + 1 + cell1[1]) % 2;
+  const cell2Black = (alpha.indexOf(cell2[0]) + 1 + cell2[1]) % 2;
+  return cell1Black === cell2Black;
+}
+
+// get the letter char index in alpha string + 1
+// add this to the number
+// see if divisible by 2
+
+
+//A1  ->  (0+1) + 1 % 2 === 0    Black
+//C3 -> (2+1) + 3 % 2 === 0  Black, true
+//A1
+//H3 -> (7+1) + 3 % 2 !== 0 
+
+chessBoardCellColor("A1", "H3")
