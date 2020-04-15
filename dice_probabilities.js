34707867a0253aa4b013fa3eedcecb36dae99da1
Zachary <zachjoliver@gmail.com>
2020-04-15 14:28:36 -0700

    Committing passing all spec version after some minor errors

--- dice_probabilities.js
+++ dice_probabilities.js
@@ -1,11 +1,26 @@
-function diceSumHelper(dice, array) {
-  let retArr = array || [];
-  
+function diceSumHelper(dice) { //2
+  if (dice ===1) return [[1],[2],[3],[4],[5],[6]];
+  const arr = diceSumHelper(dice-1);   //arr = [[1],[2],[3],[4],[5],[6]];
+  let retArr = [];
+  for (let z=0;z<arr.length;z++) {
+    for (let i=1;i<7;i++) {
+      retArr.push(arr[z].concat([i])); //[[1,1]]
+    }
+  }
+  return retArr;
 }
 
 function rolldiceSumProb(target, dice) {
-  diceSumHelper(dice);
-  return prob;
+  const arr = diceSumHelper(dice);
+  let count = 0;
+  for (let j=0;j<arr.length;j++) {
+    let total = 0;
+    for (let k=0;k<arr[j].length;k++) { //refactor to reduce
+      total += arr[j][k];
+    }
+    if (total === target) count += 1;
+  }
+  return count / Math.pow(6, dice);
 }
 
 
@@ -20,3 +35,6 @@ function rolldiceSumProb(target, dice) {
 // if we have all possibilities of n - 1 dice
 // then need to modify result to include nth dice
 // recursive helper?
+//
+
+rolldiceSumProb(11, 2);
