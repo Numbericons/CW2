a4b8b081fc31e3aa6f8beced38d8b1c621dbb738
Zachary <zachjoliver@gmail.com>
2020-04-16 12:21:27 -0700

    Passing all tests submitted version

--- beeramid.js
+++ beeramid.js
@@ -1,11 +1,14 @@
-// Returns number of complete beeramid levels
 var beeramid = function (bonus, price) {
-  const maxCans = bonus / price;
+  let cans = Math.floor(bonus / price);
   let level = 0;
 
-  while (maxCans > 0) {
-    if (maxCans > level * level) {
+  while (cans > 0) {
+    let levelSquared = (level + 1) * (level + 1);
+    if (cans >= levelSquared) {
       level += 1;
+      cans -= levelSquared;
+    } else {
+      break;
     }
   }
   return level;
@@ -14,3 +17,6 @@ var beeramid = function (bonus, price) {
 //divide bonus by price to see the maximum number of cans we could obtain
 //then level by level, track the level
 //check if the next level can be made and subtract the price of that level from maxCans
+
+
+beeramid(9, 2);
