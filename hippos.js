b3077ac5c404141a971d5280002381955e657586
Zachary <zachjoliver@gmail.com>
2020-04-17 15:13:55 -0700

    Small cleanup and refactor

--- hippos.js
+++ hippos.js
@@ -1,16 +1,13 @@
 function Game(board) {
   this.board = board;
   this.cnt = 0;
-  board.forEach(row => {
-    this.cnt += row.reduce((a,b) => a + b);
-  })
+  board.forEach(row => { this.cnt += row.reduce((a,b) => a + b) });
 }
 
 Game.prototype.chkRow = function (col, row) {
   if (!this.board[col]) return;
   for (let x=row;x<this.board.length;x++) {
     if (this.board[col][x] === 1) {
-      // if (x === 1 && col === 2) debugger;
       this.cnt -= 1;
       this.board[col][x] = 0;
         if (this.board[col-1] && this.board[col-1][x] === 1) this.chkRow(col-1,x);
