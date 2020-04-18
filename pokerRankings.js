d81e527bb0d2e9bcc24d1d6ad57abecb43ca3aff
Zachary <zachjoliver@gmail.com>
2020-04-17 17:32:25 -0700

    Set up variables and check for flush initialized and taking index within rankings of cards to sort and use to determine if straight

--- pokerRankings.js
+++ pokerRankings.js
@@ -1,14 +1,32 @@
 var Result = { "win": 1, "loss": 2, "tie": 3 }
 
 function PokerHand(hand) {
+  let cardVals = "23456789TJQKA";
   let suitsObj = { "S": 0, "H": 0, "D": 0, "C": 0 };
-  let carVals = "23456789TJQKA";
+  const cards = hand.split(' ');
+  
+  let handRanks = {};
+  let handIndex = [];
+  let straight = null;
   this.top;
   this.kick;
   this.rank;
-  const cards = hand.split(' ');
-  const suits = cards.forEach(card=> { suitsObj[card[1]] += 1 });
+
+  cards.forEach(card => {
+    if (handRanks[card[0]]) {
+      handRanks[card[0]] = handRanks[card[0]] + 1;
+      straight = false;
+    } else {
+      handRanks[card[0]] = 1;
+      handIndex.push(cardVals.indexOf(card[0]))
+    }
+    suitsObj[card[1]] += 1;
+  });
+
   const flush = suitsObj["S"] === 5 || suitsObj["H"] === 5 || suitsObj["D"] === 5 || suitsObj["C"] === 5;
+  if (!straight === false) {
+    
+  }
 }
 
 PokerHand.prototype.compareWith = function (hand) {
