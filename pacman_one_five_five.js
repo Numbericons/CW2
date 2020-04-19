88c86c2234639432420843cb5d2d9542ac6c7414
Zachary <zachjoliver@gmail.com>
2020-04-19 13:44:51 -0700

    Add pacman 155 where enemies are stagnant and need to return number of coins that can be collected without passing row or column of enemies

---
+++ pacman_one_five_five.js
@@ -0,0 +1,14 @@
+function pacMan(N, PM, enemies) {
+  //coding and coding..
+
+
+}
+
+//grid is N * N
+//PM is starting position of pac man
+//enemies is an array of enemy positions
+
+//first step is to build the board and put pacman and his enemies in proper positions
+//  all empty positions will be considered coins
+//iterate through rows and iterate through columns
+
