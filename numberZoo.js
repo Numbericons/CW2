9b5527200cd52efeab19a413024cac45b5d4ad6d
Zachary <zachjoliver@gmail.com>
2020-04-22 11:13:45 -0700

    Add number zoo and initial code that is not fast enough

---
+++ numberZoo.js
@@ -0,0 +1,21 @@
+function findNumber(array) {
+  let end = array.length;
+  for (let i=1;i<end;i++) {
+    if (!array.includes(i)) return i;
+  }
+  return end + 1;
+}
+// function findNumber(array) {
+//   let arr = array.sort((a,b) => ( a - b))
+//   for (let i=0;i<arr.length;i++) {
+//     if (arr[i] !== i+1) return i+1;
+//   }
+//   return arr[arr.length-1] + 1;
+// }
+
+// const result = findNumber([7,8,1,2,4,5,6]);
+const result = findNumber([1, 2, 3, 5]);
+console.log(result);
+
+
+//have a list with one missing element
