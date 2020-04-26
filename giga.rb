c4f27db8d542ea21290aa3287fe701e1b884189e
Zachary <zachjoliver@gmail.com>
2020-04-26 16:08:10 -0700

    Passing all specs

--- giga.rb
+++ giga.rb
@@ -1,12 +1,5 @@
 require 'byebug'
 
 def gigasecond date
-  debugger
-  # yr = date.year
-  # mnt = date.month
-  # day = date.day
-  # 10**9 / 60        / 60         / 24        / 365
-  # seconds / 60 = mins / 60 = hours / 24 = days / 365 = years
-
   Date.new(date.year, date.month, date.day) + 10**9 / 60 / 60 / 24
 end
