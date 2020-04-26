b8316720e4be6ba11ad073b2b7366340d34aa7f1
Zachary <zachjoliver@gmail.com>
2020-04-26 01:31:53 -0700

    Test methods to get name and numer from the match result sub strings

--- football.rb
+++ football.rb
@@ -1,15 +1,26 @@
 class LeagueTable
   def initialize
+    @alpha = 'abcdefghijklmnopqrstuvwxyz'
     @matches = []
     @results = Hash.new
   end
 
-  def add_result
-    
+  def getNumber(string)
+    score = ''
+    string.each_char do |c|
+      unless @alpha.include?(c.downcase)
+        name += c
+      end
+    end
+    return score.to_i
+  end
+
+  def getName(string)
+    string.split(' ').select { |str| @alpha.include?(str[0]) }.join(' ')
   end
 
   def push(match)
     @matches.push(match)
-
+    arr = match.split(' - ')
   end
 end
