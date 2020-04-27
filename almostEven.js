65c5d4e96b026a9070b7501d6992484169e0e607
Zachary <zachjoliver@gmail.com>
2020-04-27 14:53:50 -0700

    Add more logic to handle if number one less than current part would divide in easily

--- almostEven.js
+++ almostEven.js
@@ -1,21 +1,20 @@
-/**
- * Returns a [parts]-length array of equal or nearly equal
- * integers that add up to [num].
- */
 var splitInteger = function (num, parts) {
-  // Complete this function
-  if (parts === 1) return [num];
   let retArr = [];
-  let times = num / parts;
   if (num % parts === 0) {
+    let times = num / parts;
     for (let i = 0; i < parts; i++) {
       retArr.push(times);
     }
   } else {
     let part = Math.ceil(num / parts);
     while (num > 0) {
-      if (num - part > 0) {
+      if ((part - 1) % parts === 0) {
+        for (let i = 0; i < parts; i++) {
+          retArr.push(part - 1);
+        }
+      } else if (num - part > 0) {
         retArr.push(part);
+        parts -= 1;
         num -= part;
       } else {
         part -= 1;
@@ -24,3 +23,10 @@ var splitInteger = function (num, parts) {
   }
   return retArr;
 }
+
+const result = splitInteger(20, 6)  // returns [3, 3, 3, 3, 4, 4]
+console.log(result);
+
+//if num divides evenly into the number of parts return an array of number / parts with length = parts
+//else take the ceiling of the number divided by the parts
+//keep reducing the number by this amount un
