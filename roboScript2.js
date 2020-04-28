90aa3fae98c96bb1b223ffe614c22d40c3c88e38
Zachary <zachjoliver@gmail.com>
2020-04-28 14:08:32 -0700

    Add robo script two initial file with beginning of plan

---
+++ roboScript2.js
@@ -0,0 +1,17 @@
+function execute(code) {
+  let grid = [["*"]];
+  let pos = [0,0];
+  let rowlen = 1;
+  
+}
+
+
+//visited squares are a * and blank squares are a " "
+//Grid - 2d array of [row][col]
+
+//Map expansion
+//  if we try to move up/down which doesn't yet exist
+//    need to push or unshift in a new array of blank spaces the length of the present array or variable that stores this val
+//    
+
+//return by joining the rows of the 2d array with \r\n
