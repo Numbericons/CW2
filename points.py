8aa2115b95204fba6738c5fbd3ad41333b7ff90e
Zachary <zachjoliver@gmail.com>
2020-05-20 13:09:17 -0700

    Add working version of points python

--- points.py
+++ points.py
@@ -1,2 +1,10 @@
 def points(games):
+  count = 0
   for game in games:
+    if game[0] > game[2]:
+      count += 3
+    elif game[0] == game[2]:
+      count += 1
+    else:
+      count += 0
+  return count
