821879c8e141a6578e39e3bfba51fd4c28d00d71
Zachary <zachjoliver@gmail.com>
2020-05-20 13:51:48 -0700

    Add refactored clock py file

--- clock.py
+++ clock.py
@@ -1,6 +1,8 @@
 def past(h, m, s):
-    mili = 0
-    mili += s * 1000
-    mili += m * 60 * 1000
-    mili += h * 60 * 60 * 1000
-    return mili
+  return s * 1000 + m * 60 * 1000 + h * 60 * 60 * 1000
+
+  # mili = 0
+  # mili += s * 1000
+  # mili += m * 60 * 1000
+  # mili += h * 60 * 60 * 1000
+  # return mili
