8a06784f0838b4642d6ce106dce085d7214f0ea8
Zachary <zachjoliver@gmail.com>
2020-05-20 13:57:48 -0700

    Add refactored version of array plus array in python to be a single line

--- arrayPlus.py
+++ arrayPlus.py
@@ -1,4 +1,4 @@
+from functools import reduce
+
 def array_plus_array(arr1, arr2):
-  sum1 = functools.reduce(lambda a, b: a + b, arr1)
-  sum2 = functools.reduce(lambda a, b: a + b, arr2)
-  return sum1 + sum2
+  return reduce(lambda a, b: a + b, arr1) + reduce(lambda a, b: a + b, arr2)
