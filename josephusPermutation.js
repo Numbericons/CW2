e3f04599ae459f819d42f68e2ae95dad642752de
Zachary <zachjoliver@gmail.com>
2020-05-29 14:13:03 -0700

    Add initial logic to function

--- josephusPermutation.js
+++ josephusPermutation.js
@@ -1,2 +1,10 @@
 function josephus(items, k) {
+  let elim = [];
+  while (items.length) {
+    elim.push(items.splice(k % items.length,1));
+  }
+  return elim;
 }
+
+//need to return the order in which the solider were eliminated
+//  use splice return value
