5f94eba121e413ecea6694a1f890a67cdf327572
Zachary <zachjoliver@gmail.com>
2020-05-29 14:31:15 -0700

    Mid testing after several tweask to josephus

--- josephusPermutation.js
+++ josephusPermutation.js
@@ -1,10 +1,15 @@
 function josephus(items, k) {
   let elim = [];
+  let idx = k % items.length;
   while (items.length) {
-    elim.push(items.splice(k % items.length,1));
+    if (items.length === 1) idx = 1;
+    elim.push(items.splice(idx-1,1)[0]);
+    if (idx !== 0) idx -= 1;
+    idx = (idx + k) % items.length;
   }
   return elim;
 }
 
+josephus([1,2,3,4,5,6,7], 3)
 //need to return the order in which the solider were eliminated
 //  use splice return value
