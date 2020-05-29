114821b5d489473e3571418ecf87f6725cd1e778
Zachary <zachjoliver@gmail.com>
2020-05-29 16:14:50 -0700

    Add initial comments and plan to next biggest int problem

--- nextBigger.js
+++ nextBigger.js
@@ -1,3 +1,16 @@
 function nextBigger(n) {
-  //your code here
+
 }
+
+//idea 1: check all possible permutations of numbers that can be made from digits
+//  see which is next largest of possiblities
+//  could take index of initial number and take next, if it exists
+//
+
+//simple examples we can just switch the last 2 digits and see if that is larger
+
+
+//test cases to think through problem:
+//  1010
+//   560  => 650
+// 1200
