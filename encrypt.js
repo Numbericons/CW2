15afdff8db89f09a16ee57742cd52669df1dd041
Zachary <zachjoliver@gmail.com>
2020-06-08 13:38:06 -0700

    Add plans to encrpyt js

--- encrypt.js
+++ encrypt.js
@@ -5,8 +5,12 @@ var encryptThis = function (text) {
   for (let z=0; z< arr.length;z++) {
 
   }
-  
+
   return str;
 }
+
+//split text on spaces and return space seperated words
+//use map or iterate through array created by split
+//replace first char with ascii code and swap 2nd and last letters
 
 // .charCodeAt(0);
